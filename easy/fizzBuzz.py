e1712ab1efd3c68c42236a025542fe4e319b5d3a
Hubert Arciszewski <hubertarciszewski@gmail.com>
2019-04-21 14:56:12 +0200

    add function

--- easy/fizzBuzz.py
+++ easy/fizzBuzz.py
@@ -1,13 +1,22 @@
-# Napisz program FizzBuzz
-# Wypisz wszystkie liczby od 1 do 100
-# Jeśli liczba jest podzielna przez trzy wypisz “Fizz“
-# Jeśli liczba jest podzielna przez pięć wypisz “Buzz“
-# Jeśli liczba jest podzielna przez trzy i pięć wypisz “FizzBuzz“
+# You should write a function that will receive a positive integer and return:
+# "Fizz Buzz" if the number is divisible by 3 and by 5;
+# "Fizz" if the number is divisible by 3;
+# "Buzz" if the number is divisible by 5;
+# The number as a string for other cases.
 
-for x in range(1, 101):
-    if x % 3 == 0 and x % 5 == 0:
-        print(f"{x}-FizzBuzz")
-    elif x % 3 == 0:
-        print(f"{x}-Fizz")
-    elif x % 5 == 0:
-        print(f"{x}-Buzz")
+
+def fizz_buzz(number):
+    if number % 3 == 0 and number % 5 == 0:
+        return "Fizz Buzz"
+    elif number % 3 == 0:
+        return "Fizz"
+    elif number % 5 == 0:
+        return "Buzz"
+    else:
+        return str(number)
+
+
+print(fizz_buzz(15)) # "Fizz Buzz", "15 is divisible by 3 and 5"
+print(fizz_buzz(6)) # "Fizz", "6 is divisible by 3"
+print(fizz_buzz(5)) # "Buzz", "5 is divisible by 5"
+print(fizz_buzz(7)) # "7", "7 is not divisible by 3 or 5"
