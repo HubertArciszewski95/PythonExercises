f8490775767cbd3e5092cd1c66a95278a5e4da93
Hubert Arciszewski <hubertarciszewski@gmail.com>
2019-04-21 14:27:11 +0200

    add findMessage.py

---
+++ findMessage.py
@@ -0,0 +1,22 @@
+# You are given a chunk of text.
+# Gather all capital letters in one word in the order that they appear in the text.
+
+
+# First solution
+def find_message(msg):
+    secret = ""
+    for x in msg:
+        if x == x.upper() and x.isalnum():
+            secret += x
+    return secret
+
+
+# Second solution
+def find_message2(msg):
+    z = [x for x in msg if x == x.upper() and x.isalnum()]
+    return "".join(z)
+
+
+print(find_message("How are you? Eh, ok. Low or Lower? Ohhh.")) # "HELLO"
+print(find_message("hello world!")) # "", "Nothing"
+print(find_message("HELLO WORLD!!!"))  # HELLOWORLD", "Capitals"
