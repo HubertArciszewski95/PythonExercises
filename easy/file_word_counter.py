bc9a48cf219087b1d47c9c377d4b9fedf655fc3b
Hubert Arciszewski <hubertarciszewski@gmail.com>
2019-05-07 12:35:03 +0200

    Resolve problem with commas. Used .replace()

--- easy/file_word_counter.py
+++ easy/file_word_counter.py
@@ -1,10 +1,14 @@
 # Download the words1.txt file from the attachment and then create a Python function
 # that takes a text file as input and returns the number of words contained in the text file.
+# Please take into consideration that some words can be separated by a comma with no space.
+# For example "Hi,it's me." would need to be counted as three words.
 
 
 def word_counter(data):
-    return len(data.read().split())
+    read_data = data.read().replace(",", " ")
+    split_data = read_data.split()
+    return len(split_data)
 
 
-with open("/Users/hubertarciszewski/Downloads/words1.txt", "r") as data:
+with open("/Users/hubertarciszewski/Downloads/words2.txt", "r") as data:
     print(word_counter(data))
