7f3fc92266e4a0394b8a002bdbefd4b05a14b44a
Hubert Arciszewski <hubertarciszewski@gmail.com>
2019-05-08 12:34:18 +0200

    Add simple solution with for loop

--- medium/one_file_per_letter.py
+++ medium/one_file_per_letter.py
@@ -1,3 +1,7 @@
+# Please create a script that generates 26 text files named a.txt, b.txt, and so on up to z.txt.
+# Each file should contain a letter reflecting its filename.
+
+# More complex solution
 import string
 
 counter = 0
@@ -7,3 +11,8 @@ while counter <= 26:
     file = open(f"{letters[counter]}.txt", "w")
     file.write(letters[counter])
     counter += 1
+
+# Simplest version
+for x in string.ascii_lowercase:
+    file1 = open(f"{x}.txt", "w")
+    file1.write(x)
